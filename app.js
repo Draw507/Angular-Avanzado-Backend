f63918f330d6d99e7ba01ecd4a1974e0e46bd697
David Robinson <david.robinson@logicstudio.net>
2020-04-05 15:29:54 -0500

    feat: Sección 10: HospitalAPP - Backend Server - Funciones de Usuarios

--- app.js
+++ app.js
@@ -1,9 +1,20 @@
 //Requires
 var express = require('express');
 var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
 
 //Inicializar variables
 var app = express();
+
+//Body parser
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+
+//Importar rutas
+var appRoutes = require('./routes/app');
+var usuarioRoutes = require('./routes/usuarios');
+var loginRoutes = require('./routes/login');
 
 //Conexion a la base de datos
 mongoose.connection.openUri('mongodb://localhost:27017/hospitalDB', (error, response) => {
@@ -17,10 +28,7 @@ app.listen(3000, () => {
     console.log('Express server puerto 3000: \x1b[32m%s\x1b[0m', 'online');
 });
 
-//rutas
-app.get('/', (request, response, next) => {
-    response.status(200).json({
-        ok: true,
-        mensaje: 'Peticion realizada correctamente'
-    });
-});
+//Rutas
+app.use('/usuarios', usuarioRoutes);
+app.use('/login', loginRoutes);
+app.use('/', appRoutes);
---
+++ config/config.js
@@ -0,0 +1,1 @@
+module.exports.SEED = 'ng-express-seed';
